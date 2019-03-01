400c25940a2bd9093df69b44d4839af7dfcaf8c0
farkasnorbert <41139862+farkasnorbert@users.noreply.github.com>
2019-03-01 14:10:40 +0200

    Step 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,8 +16,29 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
-        System.out.print("N? : ");
+        System.out.print("N : ");
         SIZE = in.nextInt();
+        System.out.print("Megoldasi modszer:");
+        int m = in.nextInt();
+        switch (m){
+            case 1:backtracking();
+                break;
+            case 2:backtrackingMVRfc();
+                break;
+            case 3:backtrackingMVRAC3();
+                break;
+            default: break;
+        }
+    }
+
+    private static void backtrackingMVRAC3() {
+    }
+
+    private static void backtrackingMVRfc() {
+    }
+
+    private static void backtracking() {
+
     }
 
 }
